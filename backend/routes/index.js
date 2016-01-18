6c875495f05e98528ea04f61f71ced2ebc7fb03d
Julian Poyourow <julianpoyo@gmail.com>
2016-01-17 21:16:29 -0800

    Added get /prices for price check.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    CONST = require('../config/constants.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/prices', function(req, res, next) {
+    res.status(200).json(CONST.SUBSCRIPTION_PRICE);
 });
 
 module.exports = router;
