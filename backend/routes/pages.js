d76823f6fff63089c4f946ce980f60a56f11862b
Julian Poyourow <julianpoyo@gmail.com>
2016-01-17 17:42:56 -0800

    Added security to getPage

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -5,18 +5,48 @@ var Page = mongoose.model('Page');
 
 /* GET page. */
 router.get('/:number', function(req, res, next) {
-    Page.findOne({
-        number: parseInt(req.params.number)
-    }).lean().select().exec(function(err, page){
-        if(err){
-            res.status(500).send("There was an error");
-        } if(!page){
-            res.status(404).send("Page Not Found!");
-        } else {
-            page.pageContent = cleanHTML(page.content);
-            res.status(200).json(page);
-        }
+
+    SessionService.validateSession(req.params.token, "user", function(accountId) {
+        User.findById(accountId)
+        .select('name email subscription')
+        .exec(function(err, user) {
+            if (err) {
+                res.status(500).json({
+                    msg: "Couldn't search the database for user!"
+                });
+            } else if (!user) {
+                res.status(401).json({
+                    msg: "User not found, user table out of sync with session table!"
+                });
+            } else if(moment(user.subscription).isAfter(moment())){
+                res.status(402).json({
+                    msg: "Subscription expired!",
+                    subscription: user.subscription
+                });
+            } else {
+                displayPage(user);
+            }
+        });
+    }, function(err){
+        res.status(err.status).json(err);
     });
+
+    function displayPage(user){
+        Page.findOne({
+            number: parseInt(req.params.number)
+        }).lean().select().exec(function(err, page){
+            if(err){
+                res.status(500).send("There was an error");
+            } if(!page){
+                res.status(404).send("Page Not Found!");
+            } else {
+                page.content = cleanHTML(page.content);
+                page.subscription = user.subscription;
+                res.status(200).json(page);
+            }
+        });
+    }
+
     // removes MS Office generated guff
     function cleanHTML(input) {
       // 1. remove line breaks / Mso classes
