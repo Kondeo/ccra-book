ee0fcf774592a351e07c43e87034364d56e943aa
Julian Poyourow <julianpoyo@gmail.com>
2016-02-03 23:11:28 -0800

    Re-organized and cleaned searchpage logic

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -2,11 +2,6 @@ angular.module('starter')
 .controller('IndexCtrl', function($scope,
     $location, Page, loadingSpinner, Notifications) {
 
-  $scope.indexes = [
-    { title: 'Index list will be here', page: 1, id: 1, indented: 0},
-    { title: 'Index item 2', page: 1, id: 1, indented: 0}
-    ];
-
     $scope.searchResults = [];
 
     var cookie = localStorage.getItem("session_token");
@@ -14,15 +9,6 @@ angular.module('starter')
     $scope.goTo = function(page){
         $scope.temp = 'app/page/' + page;
         $location.path($scope.temp);
-    }
-
-    function findRelatives(j, width, endIndexes, startIndexes, endIndex, skip){
-        endIndex += width;
-        if(endIndexes[j] + width > startIndexes[j+1] && j+1 < startIndexes.length){
-          return findRelatives(j+1, width, endIndexes, startIndexes, endIndexes[j+1], skip + 1);
-        } else {
-          return { "end": endIndex, "skip": skip };
-        }
     }
 
     $scope.search = function(query){
@@ -49,8 +35,7 @@ angular.module('starter')
                 var endTag = "</mark>";
                 var startIndexes = getIndicesOf(startTag, results[i].highlight.content[0], false);
                 var endIndexes = getIndicesOf(endTag, results[i].highlight.content[0], false);
-                console.log(startIndexes)
-                console.log(endIndexes)
+
                 if(startIndexes.length != endIndexes.length){
                     console.log("INDEX PROBLEM. DOCUMENT ERROR.");
                     break;
@@ -66,7 +51,6 @@ angular.module('starter')
                     var chunk = results[i].highlight.content[0].substring(start, end);
                     bigFind += "..." + chunk + "...";
                     if(j+1 != startIndexes.length) bigFind += "<br />";
-                    console.log(bigFind)
                 }
 
                 results[i].highlight.content[0] = bigFind;
@@ -92,31 +76,6 @@ angular.module('starter')
         });
     }
 
-    function strip(html){
-       var tmp = document.createElement("DIV");
-       tmp.innerHTML = html;
-       return tmp.textContent || tmp.innerText || "";
-    }
-
-    function stripHTML(my_string){
-      charArr   = my_string.split('');
-      resultArr = [];
-      htmlZone  = 0;
-      quoteZone = 0;
-      for( x=0; x < charArr.length; x++ ){
-       switch( charArr[x] + htmlZone + quoteZone ){
-         case "<00" : htmlZone  = 1;break;
-         case ">10" : htmlZone  = 0;resultArr.push(' ');break;
-         case '"10' : quoteZone = 1;break;
-         case "'10" : quoteZone = 2;break;
-         case '"11' :
-         case "'12" : quoteZone = 0;break;
-         default    : if(!htmlZone){ resultArr.push(charArr[x]) }
-       }
-      }
-      return resultArr.join('')
-    }
-
     function remove_tags(html) {
      var html = html.replace(/<mark>/g,"||mark||");
      var html = html.replace(/<\/mark>/g,"||/mark||");
@@ -139,5 +98,13 @@ angular.module('starter')
           startIndex = index + searchStrLen;
       }
       return indices;
+  }
+  function findRelatives(j, width, endIndexes, startIndexes, endIndex, skip){
+      endIndex += width;
+      if(endIndexes[j] + width > startIndexes[j+1] && j+1 < startIndexes.length){
+        return findRelatives(j+1, width, endIndexes, startIndexes, endIndexes[j+1], skip + 1);
+      } else {
+        return { "end": endIndex, "skip": skip };
+      }
   }
 })
