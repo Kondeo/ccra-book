d31640b3170e71e943d1c64f7f67594e62190dc6
Julian Poyourow <julianpoyo@gmail.com>
2016-04-14 16:01:44 -0700

    Added username and password options to mongoose connection

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
 var keys = require('../config/keys.json');
-var url = "mongodb://" + keys.database.username + ":" + keys.database.password + "@localhost/ccra-book"
-mongoose.connect('mongodb://localhost/ccra-book');
+mongoose.connect('mongodb://localhost/ccra-book', {
+  user: keys.database.username,
+  pass: keys.database.password
+});
