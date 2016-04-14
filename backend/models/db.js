ea428a0f8f3a6a105c03bd74b03dd1d0650a625c
Julian Poyourow <julianpoyo@gmail.com>
2016-04-14 14:54:55 -0700

    Added new database keys to database connection

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,2 +1,4 @@
 var mongoose = require('mongoose');
+var keys = require('../config/keys.json');
+var url = "mongodb://" + keys.database.username + ":" + keys.database.password + "@localhost/ccra-book"
 mongoose.connect('mongodb://localhost/ccra-book');
