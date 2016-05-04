fee884efd663f6fb957d72aab92869a6c3225186
Julian Poyourow <julianpoyo@gmail.com>
2016-05-04 14:03:52 -0700

    Added authDB

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 var keys = require('../config/keys.json');
 mongoose.connect('mongodb://localhost/ccra-book', {
   user: keys.database.username,
-  pass: keys.database.password
+  pass: keys.database.password,
+  auth: { authdb: "admin" }
 });
